744d86f286ff2bde7f1a73a8a34990c77027f467
GochKa <gocha.workout@gmail.com>
2022-05-25 01:49:05 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 
-const {createUser, getUsers, getUser} = require("../controllers/users");
+const {createUser, getUsers, getUser, patchUser, patchAvatar} = require("../controllers/users");
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
 router.post('/users', createUser);
-//router.patch("/users/me",patchUser);
-//router.patch("/users/me/avatar", patchAvatar);
+router.patch("/users/me",patchUser);
+router.patch("/users/me/avatar", patchAvatar);
 
 module.exports = router;
 
-//, patchUser, patchAvatar
