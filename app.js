e1f131031a1d3c2e294063488f9fa842da7419e1
GochKa <gocha.workout@gmail.com>
2022-05-25 02:09:58 +0300

    Исправления

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser')
 //Установка порта
 const { PORT = 3000 } = process.env;
 
+//Подключению к базе данных
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 //Установка точки входа
 const app = express()
 
@@ -15,13 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //
-app.use((req, _, next) => {
-  req.user = {
-    _id: '628d59c4d87b3a1c6f782926',
-  };
+//app.use((req, _, next) => {
+  //req.user = {
+   // _id: '628d59c4d87b3a1c6f782926',
+  //};
 
-  next();
-});
+  //next();
+//});
 
 
 app.use('/', require('./routes/users'));
@@ -31,11 +34,8 @@ app.use('/', require('./routes/cards'));
 app.use('*', (_, res) => res.status(404).send({ message: 'Запрашиваемая страница не найдена' }));
 
 
-//Подключению к базе данных
-mongoose.connect('mongodb://localhost:27017/mestodb');
 
 
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`Приложение запущено на порту ${PORT}`)
 })
--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const createUser = (req,res) =>{
   const {name, about, avatar} = req.body
 
   return User.create({name, about, avatar})
-  .then(user => res.status(201).send({data: user}))
+  .then(user => res.send({data: user}))
   .catch(err =>{
     if(err.name === 'ValidationError'){
       return res.status(400).send({message: "Некоректные name, link или avatar"})
