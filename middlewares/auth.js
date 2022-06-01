e0937d0e74acb06ae3fdd03dcbde28443b0f313f
GochKa <gocha.workout@gmail.com>
2022-06-02 00:34:44 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/unauthorized');
 
+const { JWT_SECRET } = process.env;
+
 module.exports = (req, __, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -12,7 +14,7 @@ module.exports = (req, __, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new UnauthorizedError('Для доступа необходима авторизация');
   }
