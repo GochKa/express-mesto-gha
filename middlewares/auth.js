5e45386ff523eb56316d308e48b4a974256fa55b
GochKa <gocha.workout@gmail.com>
2022-06-02 00:20:36 +0300

    .

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,19 +3,19 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized');
 
 module.exports = (req, __, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.cookies.jwt;
+  if (!token) {
     throw new UnauthorizedError('Для доступа необходима авторизация');
   }
 
-  const token = authorization.replace('Bearer', '');
+  // const token = authorization.replace('Bearer', '');
   let payload;
 
   try {
     payload = jwt.verify(token, 'super-strong-secret');
+    req.user = payload;
+    next();
   } catch (err) {
     throw new UnauthorizedError('Для доступа необходима авторизация');
   }
-  req.user = payload;
-  next();
 };
