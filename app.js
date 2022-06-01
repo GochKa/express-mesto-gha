5f302f5dc8587ed5b0e0aa47abca11ea218f0785
GochKa <gocha.workout@gmail.com>
2022-06-01 23:47:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,14 +22,18 @@ app.post('/signup', createUser);
 
 app.use(auth);
 
-app.use('/', require('./routes/users'));
-app.use('/', require('./routes/cards'));
+app.use('/', auth, require('./routes/users'));
+app.use('/', auth, require('./routes/cards'));
 
 app.use('*', (_, __, next) => next(new NotFoundErr('Запрашиваемая страница не найдена')));
 
-app.use((err, req, res) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send(err.message);
+    return;
+  }
+  res.status(500).send({ message: 'Ошибка сервеа' });
+  next();
 });
 
 app.listen(PORT, () => {
