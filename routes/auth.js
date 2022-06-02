94a4b8559b90ed0973727ad848dadc82920821e8
GochKa <gocha.workout@gmail.com>
2022-06-03 01:12:33 +0300

    Delete auth.js

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { login, createUser } = require('../controllers/users');
-const { signinValidator, signupValidator } = require('../validators/auth');
-
-router.post('/signin', signinValidator, login);
-router.post('/signup', signupValidator, createUser);
-
-module.exports = router;
